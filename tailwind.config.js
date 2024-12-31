ba0234e5eb520a3c32e8e691ee42dae41b6ea590
CryptoLaunch123 <maxcrytpolaunch@gmail.com>
2024-12-30 20:30:41 -0500

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx}", "./index.html"],
+    theme: {
+      extend: {
+        colors: {
+          neonCyan: '#00f9ff',
+          neonMagenta: '#ff00f7',
+          neonPurple: '#9f00ff'
+        },
+        fontFamily: {
+          sans: ['Inter', 'sans-serif']
+        }
+      }
+    },
+    plugins: []
+  }
+  
